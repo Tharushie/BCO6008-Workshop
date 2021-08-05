9baeb63be890d140beabf13c12c1958fdd6f9277
Tharushie <tharushie@gmail.com>
2021-08-05 21:36:19 +1000

    New file created v1

---
+++ Workshopv1.R
@@ -0,0 +1,9 @@
+install.packages("tidyverse")
+install.packages("tidymodels")
+install.packages("skimr")
+install.packages("janitor")
+
+library(tidyverse)
+library(tidymodels)
+library(skimr)
+library(janitor)
